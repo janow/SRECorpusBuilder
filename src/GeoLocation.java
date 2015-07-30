768b4bdb69c0d8728915d877e68cc08ec3a31285
mortezakz <mortezakz@users.noreply.github.com>
2015-07-29 23:49:43 -0400

    NP1 and NP2 are geocoded using the Solr index. Information written to the HTML output. Muttiple printlns seem to make the code run slowly.

---
+++ src/GeoLocation.java
@@ -0,0 +1,110 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import edu.psu.ist.vaccine.geotxt.utils.Config;
+
+import edu.psu.ist.vaccine.geotxt.api.GeoTxtApi;
+
+public class GeoLocation {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 * @throws IllegalArgumentException
+	 * @throws ParseException
+	 */
+
+	public static String getGeoInfo(String placeName, GeoTxtApi geoTxtApi,
+			JSONParser parser) throws IllegalArgumentException,
+			URISyntaxException, IOException, ParseException {
+
+		String response = "";
+
+
+		String geoCodedString = geoTxtApi.geoCodeToGeoJson(placeName, "none",
+				false, 0, true, true);
+
+		Object geoCodedObj = parser.parse(geoCodedString);
+
+		JSONObject geoCodedJson = (JSONObject) geoCodedObj;
+
+		JSONArray features = (JSONArray) geoCodedJson.get("features");
+
+		JSONObject feature = (JSONObject) features.get(0);
+
+		JSONObject properties = (JSONObject) feature.get("properties");
+
+		Long geoNameId = (Long) properties.get("geoNameId");
+		
+		String fCode = (String) properties.get("featureCode");
+
+		if (geoNameId == null) {
+			return "No toponym found";
+		}
+
+		String toponym = (String) properties.get("toponym");
+
+		response = "<b>Toponym</b>: " + toponym;
+
+		JSONObject hierarchyFc = (JSONObject) properties.get("hierarchy");
+		JSONArray hierarchyArray = (JSONArray) hierarchyFc.get("features");
+
+		Iterator<JSONObject> iterator = hierarchyArray.iterator();
+
+		String hierarchy = "";
+
+		while (iterator.hasNext()) {
+
+			JSONObject hFeature = (JSONObject) iterator.next();
+
+			JSONObject hProperties = (JSONObject) hFeature.get("properties");
+
+			String hToponym = (String) hProperties.get("toponym");
+
+			hierarchy += hToponym + ", ";
+
+		}
+		
+		if (hierarchy.length() > 0) {
+			hierarchy = hierarchy.substring(0, hierarchy.length() - 2);
+		}
+
+		response += ", " + hierarchy;
+		
+		response += " <b> FeatureCode: </b> "+ fCode;
+
+		response += "  <a target=\"_blank\" href=\"http://www.geonames.org/"
+				+ geoNameId.toString()
+				+ "\">See on GeoNames</a> or <a target=\"_blank\" href=\"http://api.geonames.org/get?geonameId="
+				+ geoNameId.toString()
+				+ "&username=demo&style=full\">Check ID " + geoNameId + "</a>";
+		
+		
+
+		return response;
+	}
+
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException, IllegalArgumentException, URISyntaxException,
+			ParseException {
+		// TODO Auto-generated method stub
+		Config config = new Config();
+
+		GeoTxtApi geoTxtApi = new GeoTxtApi(config.getGate_home(),
+				config.getStanford_ner());
+
+		JSONParser jsonParser = new JSONParser();
+
+		System.out.println(GeoLocation.getGeoInfo("London", geoTxtApi,
+				jsonParser));
+
+	}
+
+}
--- src/Test.java
+++ src/Test.java
@@ -11,9 +11,13 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Logger;
+
+import org.json.simple.parser.JSONParser;
 
 import edu.psu.ist.vaccine.analyzers.GateHierarchyAnalyzer;
 import edu.psu.ist.vaccine.analyzers.StanfordHierarchyAnalyzer;
+import edu.psu.ist.vaccine.corpusbuilding.CorpusExposerApi;
 import edu.psu.ist.vaccine.geotxt.api.GeoTxtApi;
 import edu.psu.ist.vaccine.geotxt.utils.Config;
 import edu.stanford.nlp.ling.CoreLabel;
@@ -37,35 +41,51 @@ import au.com.bytecode.opencsv.CSVReader;
 
 public class Test {
 
-	public static void parseFile(String filename, String outfile) throws FileNotFoundException, IOException {
+	public static final Logger log = Logger.getLogger(Test.class.getName());
+
+	public static void parseFile(String filename, String outfile)
+			throws FileNotFoundException, IOException {
 		Config config = new Config();
-		
-		LexicalizedParser lp = LexicalizedParser.loadModel("englishPCFG.ser.gz");
-		TokenizerFactory<CoreLabel> tokenizerFactory =
-				PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
-		GeoTxtApi geoTxtApi = new GeoTxtApi(config.getGate_home(), config.getStanford_ner());
-		
-		
+
+		LexicalizedParser lp = LexicalizedParser
+				.loadModel("englishPCFG.ser.gz");
+		TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(
+				new CoreLabelTokenFactory(), "");
+		GeoTxtApi geoTxtApi = new GeoTxtApi(config.getGate_home(),
+				config.getStanford_ner());
+
+		JSONParser jsonParser = new JSONParser();
+
 		try {
-			CSVReader r = new CSVReader (new InputStreamReader(new FileInputStream(filename), "UTF-8"));
-			
+			CSVReader r = new CSVReader(new InputStreamReader(
+					new FileInputStream(filename), "UTF-8"));
+
 			BufferedWriter bw = new BufferedWriter(new FileWriter(outfile));
 			bw.write("<!DOCTYPE html><html><head></head><body>\n");
-			
+
 			int cc = 0;
+			
 
 			String[] rows = r.readNext();
 			String line = rows[6];
 			while (line != null) {
+				
+				
 
-				if (!line.replaceFirst("^[\\x00-\\x200\\xA0]+", "").replaceFirst("[\\x00-\\x20\\xA0]+$", "").isEmpty()) {
-					//line = "Timhotel Paris Boulogne is located in Boulogne-Billancourt, close to Pierre de Coubertin Stadium, Eiffel Tower, and Stade de Roland Garros.";
-					
-					
+				if (!line.replaceFirst("^[\\x00-\\x200\\xA0]+", "")
+						.replaceFirst("[\\x00-\\x20\\xA0]+$", "").isEmpty()) {
+					// line =
+					// "Timhotel Paris Boulogne is located in Boulogne-Billancourt, close to Pierre de Coubertin Stadium, Eiffel Tower, and Stade de Roland Garros.";
+
 					// tokenize and produce parse tree
-					Tokenizer<CoreLabel> tok = tokenizerFactory.getTokenizer(new StringReader(line));
-					//Tokenizer<CoreLabel> tok = tokenizerFactory.getTokenizer(new StringReader("Hotel Jardin d'Eiffel is close to Eiffel tower"));
-					//Tokenizer<CoreLabel> tok = tokenizerFactory.getTokenizer(new StringReader("Situated in Paris's Trocadero neighborhood, this hotel is close to Wine Museum, Eiffel Tower, and Arc de Triomphe"));
+					Tokenizer<CoreLabel> tok = tokenizerFactory
+							.getTokenizer(new StringReader(line));
+					// Tokenizer<CoreLabel> tok =
+					// tokenizerFactory.getTokenizer(new
+					// StringReader("Hotel Jardin d'Eiffel is close to Eiffel tower"));
+					// Tokenizer<CoreLabel> tok =
+					// tokenizerFactory.getTokenizer(new
+					// StringReader("Situated in Paris's Trocadero neighborhood, this hotel is close to Wine Museum, Eiffel Tower, and Arc de Triomphe"));
 
 					List<CoreLabel> rawWords2 = tok.tokenize();
 
@@ -75,70 +95,102 @@ public class Test {
 					TreePrint tp = new TreePrint("penn,typedDependencies");
 					StringWriter sw = new StringWriter();
 					PrintWriter writer = new PrintWriter(sw);
-					//System.out.println("sentence: >"+line+"<");
-					//tp.printTree(parse,writer);
-					//System.out.println("Parse results:\n"+sw.toString());
+					
+					
+					// System.out.println("sentence: >"+line+"<");
+					// tp.printTree(parse,writer);
+					// System.out.println("Parse results:\n"+sw.toString());
 
 					// test tregex by checking for a pattern
 					String s = "S < NP=np1 <+(VP) (ADVP < (RB < close) < (PP < (TO < to) <+(NP) (NP=np2 !< CC)))";
-					 //String s = "S < NP=np1 < (VP < (ADJP < (JJ < close)))";
-					
-					
+					// String s = "S < NP=np1 < (VP < (ADJP < (JJ < close)))";
+
 					TregexPattern p = TregexPattern.compile(s);
 					TregexMatcher m = p.matcher(parse);
-					
+
 					int start = Integer.parseInt(rows[4]);
 					int end = Integer.parseInt(rows[5]);
-					
-					
+
 					while (m.find()) {
-						System.out.println("sentence: >"+line+"<");								
-						bw.write("<div><h2>"+line.substring(0,start)+"<u>"+line.substring(start,end)+"</u>"+line.substring(end)+"</h2>\n");
-						
-//						System.out.println("geotxt: ");
-//						String geocodeResults = geoTxtApi.geoCodeToGeoJson(line, "stanfordh", false, 0, false, true);
-//						System.out.println(geocodeResults);
-//						System.out.println("stanford extraction: ");
-//						String sr = StanfordHierarchyAnalyzer.st.tagAlltoGeoJson(line, false, 0, false, true);
-//						System.out.println(sr);
-//						System.out.println("gate extraction: ");
-//						sr = GateHierarchyAnalyzer.gate.tagAlltoGeoJson(line, false, 0, false, true);
-//						System.out.println(sr);
+						// System.out.println("sentence: >" + line + "<");
+						bw.write("<div><h2>" + line.substring(0, start) + "<u>"
+								+ line.substring(start, end) + "</u>"
+								+ line.substring(end) + "</h2>\n");
+
+						// System.out.println("geotxt: ");
+						// String geocodeResults =
+						// geoTxtApi.geoCodeToGeoJson(line, "stanfordh", false,
+						// 0, false, true);
+						// System.out.println(geocodeResults);
+						// System.out.println("stanford extraction: ");
+						// String sr =
+						// StanfordHierarchyAnalyzer.st.tagAlltoGeoJson(line,
+						// false, 0, false, true);
+						// System.out.println(sr);
+						// System.out.println("gate extraction: ");
+						// sr = GateHierarchyAnalyzer.gate.tagAlltoGeoJson(line,
+						// false, 0, false, true);
+						// System.out.println(sr);
 						Tree t = m.getNode("np1");
-						System.out.println("np1:\n"+Sentence.listToString(t.yield()));
-						bw.write("<p>np1: " + Sentence.listToString(t.yield())+"</p>\n");
-						
+						// System.out.println("np1:\n"
+						// + Sentence.listToString(t.yield()));
+						bw.write("<p>np1: " + Sentence.listToString(t.yield())
+								+ "</p>\n");
+						// System.out.println("<p>toponym: "
+						// + GeoLocation.getGeoInfo(
+						// Sentence.listToString(t.yield()),
+						// geoTxtApi, jsonParser) + "</p>\n");
+
+						bw.write("<p>toponym: "
+								+ GeoLocation.getGeoInfo(
+										Sentence.listToString(t.yield()),
+										geoTxtApi, jsonParser) + "</p>\n");
+
 						t = m.getNode("np2");
-						System.out.println("np2:\n"+Sentence.listToString(t.yield()));
-						bw.write("<p>np2: " + Sentence.listToString(t.yield())+"</p>\n");
-						
-						//System.out.println("match:\n")
+						// System.out.println("np2:\n"
+						// + Sentence.listToString(t.yield()));
+						bw.write("<p>np2: " + Sentence.listToString(t.yield())
+								+ "</p>\n");
+						// System.out.println("<p>toponym: "
+						// + GeoLocation.getGeoInfo(
+						// Sentence.listToString(t.yield()),
+						// geoTxtApi, jsonParser) + "</p>\n");
+
+						bw.write("<p>toponym: "
+								+ GeoLocation.getGeoInfo(
+										Sentence.listToString(t.yield()),
+										geoTxtApi, jsonParser) + "</p>\n");
+
+						// System.out.println("match:\n")
 						m.getMatch().pennPrint();
-						System.out.println("\n");
-						bw.write("<p><pre>"+m.getMatch().pennString()+"</pre></p>\n");
-						
+						// System.out.println("\n");
+						bw.write("<p><pre>" + m.getMatch().pennString()
+								+ "</pre></p>\n");
+
 						bw.write("</div>\n");
-						cc++;
+						
 					}
 				}
+				cc++;
+				log.info(Integer.toString(cc) +" processed.");
 				rows = r.readNext();
 				line = rows[6];
 			}
-			System.out.println(cc);
+			log.info(Integer.toString(cc));
 			r.close();
 			bw.write("</body></html>");
 			bw.close();
 
 		} catch (Exception e) {
-			System.out.println("file operation failed, could not read file");
+			log.info("file operation failed, could not read file");
 			e.printStackTrace();
 		}
 	}
 
 	public final static void main(String[] args) throws Exception {
 
-		parseFile("phrases_20150716.csv","parse_results.html");
-		//parseFile("phrases_cities_20150708.txt","parse_results.html");
+		parseFile("phrases_20150716.csv", "parse_results.html");
+		// parseFile("phrases_cities_20150708.txt","parse_results.html");
 	}
 
 }
